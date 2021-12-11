234fc46e8cddc6138f5ebe0476515515d3aab3bf
merinjo90 <merinjo90@gmail.com>
2021-12-10 22:08:28 -0500

    user register API

--- pythonProject/CRUDAPI/crudapi/crudapi/urls.py
+++ pythonProject/CRUDAPI/crudapi/crudapi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from articleapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register',views.Register.as_view(),name='Register')
 ]
